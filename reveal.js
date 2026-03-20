71f692100bf56430343b651693b406d57508056a
JoaoGabriel038 <joaogabriel038@gmail.com>
2026-03-20 15:45:49 -0300

    animacoes

---
+++ reveal.js
@@ -0,0 +1,40 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.efeito-txt-topo', {
+    duration: 2000,
+    distance: '90px',
+})
+
+revelar.reveal('.txt-sobre', {
+    duration: 2000,
+    distance: '90px',
+})
+
+revelar.reveal('.titulo-section', {
+    duration: 1000,
+    distance: '90px',
+})
+
+revelar.reveal('.box1', {
+    duration: 1000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.box2', {
+    duration: 1000,
+    distance: '90px',
+    delay: 800
+})
+
+revelar.reveal('.box3', {
+    duration: 1000,
+    distance: '90px',
+    delay: 1100
+})
+
+revelar.reveal('.box4', {
+    duration: 1000,
+    distance: '90px',
+    delay: 1400
+})
